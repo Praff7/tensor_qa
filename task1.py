0b77593f6dd0f57f800e0278936eecd5a2014356
Praff7 <praff7@gmail.com>
2023-05-01 00:22:35 +0300

    изменение в тест

--- task1.py
+++ task1.py
@@ -1,1 +1,9 @@
-test
+my_name = 'Роман'
+my_surname = 'Панов'
+experience = '13'
+post = 'инженер-тестировщик'
+print('Резюме')
+print('Имя:', my_name)
+print('Фамилия:', my_surname)
+print('Стаж работы в компании:', experience, 'лет')
+print('Должность:', post)
